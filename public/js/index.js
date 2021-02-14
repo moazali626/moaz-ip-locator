183a24a9efe4cf5f5c35ec37d9ab1972fa3dbb70
Moaz Ali <moazali626@gmail.com>
2021-02-14 14:17:01 +0500

    initial 14 feb commit

---
+++ public/js/index.js
@@ -0,0 +1,59 @@
+document.querySelector("#loading").textContent = "FETCHING DATA...";
+
+function IP(value) {
+  if (value) {
+    var string = value;
+  } else {
+    var string = "";
+  }
+  fetch(
+    `http://ip-api.com/json/${string}?fields=country,countryCode,regionName,city,zip,lat,lon,timezone,currency,isp,proxy,query`
+  ).then((response) => {
+    response.json().then((result) => {
+      document.querySelector("#loading").textContent = "";
+      document.querySelector("#home-ip").textContent = result.query;
+      document.querySelector("#query").textContent = result.query;
+      document.querySelector("#country").textContent = result.country;
+      document.querySelector("#countryCode").textContent = result.countryCode;
+      document.querySelector("#currency").textContent = result.currency;
+      document.querySelector("#regionName").textContent = result.regionName;
+      document.querySelector("#city").textContent = result.city;
+      document.querySelector("#zip").textContent = result.zip;
+      document.querySelector("#timezone").textContent = result.timezone;
+      document.querySelector("#isp").textContent = result.isp;
+
+      var proxy = document.querySelector("#proxy");
+      if (result.proxy) {
+        proxy.textContent = "Yes";
+      } else {
+        proxy.textContent = "No";
+      }
+
+      document.querySelector("#lat").textContent = result.lat;
+      document.querySelector("#lon").textContent = result.lon;
+
+      var container = L.DomUtil.get("mapid");
+      if (container != null) {
+        container._leaflet_id = null;
+      }
+
+      var mymap = L.map("mapid").setView([result.lat, result.lon], 12);
+      const attribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+      const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+      const tiles = L.tileLayer(tileUrl, { attribution });
+      tiles.addTo(mymap);
+      L.marker([result.lat, result.lon]).addTo(mymap);
+    });
+  });
+}
+
+IP();
+
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  IP(search.value);
+});
---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.port || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../views");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running at port ${port} `);
+});
